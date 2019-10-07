0b399766c7d3f5fc3174e729c734dd0325f0ab97
ajautz@gmail.com <ajautz@gmail.com>
2019-10-07 17:15:42 -0400

    Ran into a H10 error with Heroku

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-#check one, two
+import os
 from flask import Flask, escape, request, render_template
 import json
+
 
 app = Flask(__name__)
 
@@ -35,5 +36,6 @@ def search():
 #     return f'Hello, {escape(name)}!'
 
 if __name__ == "__main__":
-	app.run()
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host='0.0.0.0', port=port)
 
