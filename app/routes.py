a0e07f993545d5961b6270733e08e023f27dffef
ajautz@gmail.com <ajautz@gmail.com>
2019-10-09 19:22:39 -0400

    working on the favorites

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,8 @@ import json
 import requests
 from requests.exceptions import HTTPError
 from flask_bootstrap import Bootstrap
-from conf import Config
+#from conf import Config, Conf, Favs
+import conf
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -24,11 +25,18 @@ def index():
 #     with open(filename) as data_file:
 #         data = json.load(data_file)
 #         return data
+#     return render_template('favorites.html')
 
-@app.route('/favorites')
+@app.route('/addfavorite')
 def favorites():
     """if query params are passed, write movie to json file."""
-    return render_template('favorites.html')
+    data = {}
+    data['favorites'] = []
+    newfavorite = request.href['title']
+    print(newfavorite)
+    data['favorites'].append(newfavorite)
+    with open('data.json', 'w') as outfile:
+            json.dump(data, outfile)
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -36,29 +44,29 @@ def search():
     # include some validation on the search input
     searchtype = 'search'
     query = request.form['title']
-    #if query.validate_on_submit():
-    #    flash('Search cannot be blank')
-    print(query)
+    with open('config.json') as datafile:
+            data = json.load(datafile)
+            appid = data['omdb_api_key']
+            #print(appid)
+    #print("Query: ",query)
     if searchtype == 'search': param = 's'
-    url = "http://www.omdbapi.com/?apikey=815bb1ac&{}={}".format(param,query)
+    url = "http://www.omdbapi.com/?apikey={}&{}={}".format(appid,param,query)
     x = requests.get(url)
     results = json.loads(x.text)
     results = results['Search']
-    print(type(results))
+    #print(type(results))
     
-    return render_template('search_results.html', title=query, searchResults=results) #f'Hello, you searched for {movie_title} by {movie_director}!' # this should actually go to the search results page template
+    return render_template('search_results.html', title=query, searchResults=results) 
 
 @app.route('/movie/<movie_imdbID>')
 def movie_detail(movie_imdbID):
     """if fetch data from movie database by oid and display info."""
-    #qs_name = request.args.get('name', '')
-    #qs_oid = request.args.get('oid', '')
     param = 'i'
     query = movie_imdbID
-    print("IMDB ID: ",query)
+    #print("IMDB ID: ",query)
     url = "http://www.omdbapi.com/?apikey=815bb1ac&{}={}".format(param,query)
     x = requests.get(url)
     results = json.loads(x.text)
     title = results['Title']
     #print('Results: ', results)
-    return render_template('movie.html', title=title, movieSelect=results) #f'Hello, {escape(name)}!'
+    return render_template('movie.html', title=title, movieSelect=results)
--- conf.py
+++ conf.py
@@ -13,6 +13,18 @@ class Conf:
             # return value of supplied key
             return self.__dict__.get(k, None)
 
+class Favs:
+        def __init__(self):
+            # load and store configuration and update object dictionary
+            with open('data.json') as d:
+                fav_data = json.load(d)
+            self.__dict__.update(fav_data)
+        
+        def __getitem__(self, m):
+            # return value of supplied key
+            return self.__dict__.get(m, None)
+
+
 class Config(object):
     # configuration for backend
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
