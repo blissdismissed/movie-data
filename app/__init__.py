8dca9ec5a8f2f4af2d545a45d245e2b32f90a332
ajautz@gmail.com <ajautz@gmail.com>
2019-10-10 16:11:49 -0400

    deploy to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 from flask import Flask
 from conf import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -12,5 +15,24 @@ bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
+def create_app(config_class=Config):
+    # check configuration
+    if not app.debug and not app.testing:
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exits('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
 
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Movie Search Startup')
+
+    return app
 
